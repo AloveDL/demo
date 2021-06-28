a1c7a974e64a55653626de7c471be97ade70c348
AloveDL <2763570837@qq.com>
2021-06-28 17:00:36 +0800

    update file

---
+++ mainwindow.cpp
@@ -0,0 +1,22 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    myid = new QLabel(this);
+    myid->setText(tr("2018214789"));
+    myid->setGeometry(0,0,1800,100);
+    myid->setStyleSheet("color:black");
+    myid->setStyleSheet("background:rgb(240,245,240)");
+    myid->setContentsMargins(400,0,0,0);
+}
+
+MainWindow::~MainWindow()
+{
+
+    delete ui;
+}
+
